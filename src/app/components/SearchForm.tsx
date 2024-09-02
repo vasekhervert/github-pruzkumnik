1858d56404186c2d3a34533fbe351ef4409b951d
Vasek Hervert <vasekhervert@gmail.com>
2024-09-02 13:36:40 +0200

    added useForm, validation

--- src/app/components/SearchForm.tsx
+++ src/app/components/SearchForm.tsx
@@ -1,34 +1,44 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { SearchParamsType } from "./utils";
 
 const SearchForm = ({ searchParams }: { searchParams: SearchParamsType }) => {
-  const [query, setQuery] = useState(searchParams.q ?? "");
   const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      query: searchParams.q ?? "",
+    },
+  });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
-  };
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    router.push(`?q=${encodeURIComponent(query)}`);
+  const onSubmit = (data: { query: string }) => {
+    router.push(`?q=${encodeURIComponent(data.query)}`);
   };
 
   return (
-    <form onSubmit={handleSubmit} className="mb-8 flex">
-      <input
-        type="text"
-        value={query}
-        onChange={handleInputChange}
-        placeholder="Zadejte název repozitáře"
-        className="w-full p-2 border text-black font-bold border-gray-300 rounded me-2"
-      />
+    <form onSubmit={handleSubmit(onSubmit)} className="mb-8 flex gap-4">
+      <div className="flex flex-col flex-grow relative">
+        <input
+          type="text"
+          {...register("query", { required: true, minLength: 2 })}
+          placeholder="Zadejte název repozitáře"
+          className="w-full p-2 border text-black font-bold border-gray-300 rounded me-2"
+        />
+        {errors.query && (
+          <span className="text-red-500 absolute bottom-[-30px] left-0">
+            {errors.query.type === "required" && "Toto pole je povinné."}
+            {errors.query.type === "minLength" && "Minimální délka je 2 znaky."}
+          </span>
+        )}
+      </div>
       <button
         type="submit"
-        className=" px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 self-center"
       >
         Hledat
       </button>
