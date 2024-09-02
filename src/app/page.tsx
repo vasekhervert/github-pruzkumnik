d734660d194d4f0f0bec05d7a902510334ef92d3
Vasek Hervert <vasekhervert@gmail.com>
2024-09-02 11:31:15 +0200

    added ApolloProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import SearchForm from "./components/SearchForm";
 import RepositoryList from "./components/RepositoryList";
 import { parseSearchParams } from "./components/utils";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./lib/apollo-client";
 
 export default function Home({
   searchParams,
@@ -8,13 +12,16 @@ export default function Home({
   searchParams?: Record<string, string | string[] | undefined>;
 }) {
   const sp = parseSearchParams(searchParams ?? {});
+
   return (
     <main className="container mx-auto max-w-2xl px-4 py-8 min-h-screen">
       <h1 className="text-3xl font-bold text-gray-300 mb-8 text-center">
         GitHub Průzkumník
       </h1>
       <SearchForm searchParams={sp} />
-      <RepositoryList searchParams={sp} />
+      <ApolloProvider client={client}>
+        <RepositoryList searchParams={sp} />
+      </ApolloProvider>
     </main>
   );
 }
